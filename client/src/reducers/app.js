8e4a66b5e3b390422c5ea1515cd14611dd7ed461
hotyes <hotyes@outlook.com>
2020-02-11 23:54:59 +0800

    change default lang

--- client/src/reducers/app.js
+++ client/src/reducers/app.js
@@ -1,66 +1,68 @@
-import Cookie from 'js-cookie';
-import {getTranslations} from 'i18n';
+import Cookie from "js-cookie";
+import { getTranslations } from "i18n";
 
-const language = Cookie.get('language') || 'en';
+const language = Cookie.get("language") || "zhCN";
 
 const initialState = {
-  modalComponent: null,
-  scrolledToBottom: true,
-  windowIsFocused: true,
-  unreadMessageCount: 0,
-  soundIsEnabled: true,
-  socketConnected: false,
-  language,
-  translations: getTranslations(language)
-}
+    modalComponent: null,
+    scrolledToBottom: true,
+    windowIsFocused: true,
+    unreadMessageCount: 0,
+    soundIsEnabled: true,
+    socketConnected: false,
+    language,
+    translations: getTranslations(language)
+};
 
 const app = (state = initialState, action) => {
-  switch (action.type) {
-    case 'OPEN_MODAL':
-      return {
-        ...state,
-        modalComponent: action.payload,
-      }
-    case 'CLOSE_MODAL':
-      return {
-        ...state,
-        modalComponent: null,
-      }
-    case 'SET_SCROLLED_TO_BOTTOM':
-      return {
-        ...state,
-        scrolledToBottom: action.payload,
-      }
-    case 'TOGGLE_WINDOW_FOCUS':
-      return {
-        ...state,
-        windowIsFocused: action.payload,
-        unreadMessageCount: 0,
-      }
-    case 'RECEIVE_ENCRYPTED_MESSAGE_TEXT_MESSAGE':
-      return {
-        ...state,
-        unreadMessageCount: state.windowIsFocused ? 0 : state.unreadMessageCount + 1,
-      }
-    case 'TOGGLE_SOUND_ENABLED':
-      return {
-        ...state,
-        soundIsEnabled: action.payload,
-      }
-    case 'TOGGLE_SOCKET_CONNECTED':
-      return {
-        ...state,
-        socketConnected: action.payload,
-      }
-    case 'CHANGE_LANGUAGE':
-      return {
-        ...state,
-        language: action.payload,
-        translations: getTranslations(action.payload)
-      }
-    default:
-      return state
-  }
-}
+    switch (action.type) {
+        case "OPEN_MODAL":
+            return {
+                ...state,
+                modalComponent: action.payload
+            };
+        case "CLOSE_MODAL":
+            return {
+                ...state,
+                modalComponent: null
+            };
+        case "SET_SCROLLED_TO_BOTTOM":
+            return {
+                ...state,
+                scrolledToBottom: action.payload
+            };
+        case "TOGGLE_WINDOW_FOCUS":
+            return {
+                ...state,
+                windowIsFocused: action.payload,
+                unreadMessageCount: 0
+            };
+        case "RECEIVE_ENCRYPTED_MESSAGE_TEXT_MESSAGE":
+            return {
+                ...state,
+                unreadMessageCount: state.windowIsFocused
+                    ? 0
+                    : state.unreadMessageCount + 1
+            };
+        case "TOGGLE_SOUND_ENABLED":
+            return {
+                ...state,
+                soundIsEnabled: action.payload
+            };
+        case "TOGGLE_SOCKET_CONNECTED":
+            return {
+                ...state,
+                socketConnected: action.payload
+            };
+        case "CHANGE_LANGUAGE":
+            return {
+                ...state,
+                language: action.payload,
+                translations: getTranslations(action.payload)
+            };
+        default:
+            return state;
+    }
+};
 
-export default app
+export default app;
